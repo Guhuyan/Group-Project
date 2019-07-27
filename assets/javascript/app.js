3022fc38df9523ffa4c9934a3dc2e3193a7e1d1c
Billy Bright <billybright1992@gmail.com>
2019-07-27 14:00:52 -0700

    got cover images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
     // When the document is finished loading, start the clock.
     function startClock() {
@@ -15,10 +15,10 @@ $( document ).ready(function() {
     startClock();
     //
 
-    $("#submit").on("click", function() {
+    $("#submit").on("click", function () {
         event.preventDefault();
         let userInput = $("#user-input").val().trim();
-        let queryURL = `https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games/?search=${userInput}&fields=id,name`;
+        let queryURL = `https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games/?search=${userInput}&fields=id,name,url,summary,cover.*`;
         $.ajax({
             url: queryURL,
             method: 'POST',
@@ -26,11 +26,13 @@ $( document ).ready(function() {
                 'user-key': '73e145ac8c0ebbcea1ebea4527dba99e',
             },
         }).then(response => {
-                console.log(response);
-            })
-            .catch(err => {
-                console.error(err);
-            });
+            console.log(response);
+
+        })
+        .catch(err => {
+            console.error(err);
+        });
+
     });
 
 });
