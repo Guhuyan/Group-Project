a0e91ff0d21fa01c6bc3e0ba3b4868c6e7a68ec8
Gue <guehyang@gmail.com>
2019-07-27 11:55:03 -0700

    We made the IGDB API work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,36 @@
 $( document ).ready(function() {
 
-// When the document is finished loading, start the clock.
-function startClock() {
-    let clock = setInterval(displayClock, 1000);
-}
-function returnTime() {
-    let time = moment();
-    let readableTime = time.format('LLLL');
-    return readableTime;
-}
-function displayClock() {
-    $("#clock").text(returnTime());
-}
-startClock();
-//
+    // When the document is finished loading, start the clock.
+    function startClock() {
+        let clock = setInterval(displayClock, 1000);
+    }
+    function returnTime() {
+        let time = moment();
+        let readableTime = time.format('LLLL');
+        return readableTime;
+    }
+    function displayClock() {
+        $("#clock").text(returnTime());
+    }
+    startClock();
+    //
 
+    $("#submit").on("click", function() {
+        event.preventDefault();
+        let userInput = $("#user-input").val().trim();
+        let queryURL = `https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games/?search=${userInput}&fields=id,name`;
+        $.ajax({
+            url: queryURL,
+            method: 'POST',
+            headers: {
+                'user-key': '73e145ac8c0ebbcea1ebea4527dba99e',
+            },
+        }).then(response => {
+                console.log(response);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    });
 
 });
