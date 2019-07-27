2196317a6762411ce3892ba451df77e05d7d8a3b
Carl Medina <carl.l.medina@comcast.net>
2019-07-27 13:13:29 -0700

    added font styles in css and added minor changes to layout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,20 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
-// When the document is finished loading, start the clock.
-function startClock() {
-    let clock = setInterval(displayClock, 1000);
-}
-function returnTime() {
-    let time = moment();
-    let readableTime = time.format('LLLL');
-    return readableTime;
-}
-function displayClock() {
-    $("#clock").text(returnTime());
-}
-startClock();
-//
+    // When the document is finished loading, start the clock.
+    function startClock() 
+    {
+        let clock = setInterval(displayClock, 0);
+    }
+    function returnTime() {
+        let time = moment();
+        let readableTime = time.format('LLLL');
+        return readableTime;
+    }
+    function displayClock() {
+        $("#clock").text(returnTime());
+    }
+    startClock();
+    //
 
 
 });
